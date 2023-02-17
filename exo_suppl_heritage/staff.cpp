4d2a2ed2d605f2bf3f5987d051198eff68c340bc
Fendrul <53517259+Fendrul@users.noreply.github.com>
2023-02-17 18:57:21 +0100

    Update staff.cpp

--- exo_suppl_heritage/staff.cpp
+++ exo_suppl_heritage/staff.cpp
@@ -15,7 +15,7 @@ Staff::Staff(const std::string& name, const std::string& surname, int age) {
         staffInitialized = true;
     }
 
-    Staf::nbStaff++;
+    Staff::nbStaff++;
 
     if (nbStaff = Staf::listStaffSize) {
         Staff::resizeListStaff();
