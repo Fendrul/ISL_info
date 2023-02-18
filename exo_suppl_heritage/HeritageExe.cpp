38f555175c5ff9e834db03e00c8b8e3f23d06614
Simon <simon.strykers@gmail.com>
2023-02-18 17:34:48 +0100

    clang-tidy pour auto

--- exo_suppl_heritage/HeritageExe.cpp
+++ exo_suppl_heritage/HeritageExe.cpp
@@ -11,11 +11,11 @@ using namespace std;
 
 int startHeritageExe() {
 
-    Employee *employee1 = new Employee("Simon", "Bouvier", 20, 1000, 40);
-    Employee *employee2 = new Employee("Rachelle", "Doutier", 20, 1200, 35);
-    Employee *employee3 = new Employee("Paul", "Ravier", 20, 850, 62);
-    Manager *manager1 = new Manager("Jean", "Dupont", 20, 1500, 30);
-    Manager *manager2 = new Manager("Pierre", "Boulard", 20, 2000, 70);
+    auto *employee1 = new Employee("Simon", "Bouvier", 20, 1000, 40);
+    auto *employee2 = new Employee("Rachelle", "Doutier", 20, 1200, 35);
+    auto *employee3 = new Employee("Paul", "Ravier", 20, 850, 62);
+    auto *manager1 = new Manager("Jean", "Dupont", 20, 1500, 30);
+    auto *manager2 = new Manager("Pierre", "Boulard", 20, 2000, 70);
 
     manager1->addSubordinate(employee1);
     manager1->addSubordinate(employee2);
@@ -29,8 +29,8 @@ int startHeritageExe() {
     cout << "Direct and indirect subordinates of the highest manager\n" << endl;
     manager2->showAllSubordinates();
 
-    Commercial *commercial1 = new Commercial("Daniyal", "Yunusau", 26, 1250, 20, 80);
-    Commercial *commercial2 = new Commercial("Charlotte", "Meyer", 27, 1500, 30, 60);
+    auto *commercial1 = new Commercial("Daniyal", "Yunusau", 26, 1250, 20, 80);
+    auto *commercial2 = new Commercial("Charlotte", "Meyer", 27, 1500, 30, 60);
 
     cout << "Le salaire total de tous les employés est " << Staff::calculateAllStaffSalary() << endl;
 
